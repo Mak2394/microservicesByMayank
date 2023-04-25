da5852b3b7b9b8d1190ca880b31790543cc6aaea
Mayank Gaur <AGL IT@DESKTOP-5BU1LTE>
2023-04-25 16:22:10 +0530

    Microservices

---
+++ JWT_Token/JWT_Token/Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using JWT_Token.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Microservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [Authorize]
+        [HttpGet]
+        public string GetData()
+        {
+            return "Authenticate with JWT";
+        }
+        [HttpGet]
+        [Route("Details")]
+        public string Details()
+        {
+            return "Authenticate with JWT";
+        }
+
+
+
+        [Authorize]
+        [HttpPost]
+        public string Adduser(Users user) 
+        {
+            return "User added with username" + user.Username;
+             
+        }
+
+    }
+}
---
+++ JWT_Token/JWT_Token/Models/Users.cs
@@ -0,0 +1,8 @@
+﻿namespace JWT_Token.Models
+{
+    public class Users
+    {
+        public String? Username { get; set; }
+        public String? Password { get; set; } 
+    }
+}
